cf0a7786e3a686c138d988d19bd8d050352827b8
Sébastien <sebtiz13@gmail.com>
2021-07-29 22:31:23 +0200

    feat(frontend): implement base request on frontend

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    {{ characters }}
+  </div>
+</template>
+
+<script>
+import axios from '@/utils/axios'
+
+export default {
+  name: 'Home',
+  data: () => ({
+    total: 0,
+    count: 0,
+    characters: [],
+  }),
+  mounted () {
+    this.loadData()
+  },
+  methods: {
+    async loadData () {
+      const { data: { data }, status } = await axios.get('/characters')
+      if (status === 200) {
+        this.total = data.total
+        this.count = data.count
+        this.characters = data.results
+      }
+    },
+  },
+}
+</script>
