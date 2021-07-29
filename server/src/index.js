54af6e2288de0ffbf0989ea94a0edda7b4800758
Sébastien <sebtiz13@gmail.com>
2021-07-29 22:28:45 +0200

    feat(backend): serve example file

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,17 @@
 import express from 'express'
 import dotenv from 'dotenv-flow'
-import axios from './utils/axios.js'
+// import axios from './utils/axios.js'
+import fs from 'fs'
+import path from 'path'
 
 dotenv.config()
 const app = express()
 const port = 3000
 
 app.get('/characters', async (req, res) => {
-  const { data } = await axios.get('/characters')
+  const exampleFile = path.join(process.cwd(), 'example.json')
+  const data = JSON.parse(fs.readFileSync(exampleFile, 'utf8'))
+  // const { data } = await axios.get('/characters')
   return res.header('Access-Control-Allow-Origin', '*')
     .json(data)
 })
