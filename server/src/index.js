4c3f7fbfbef590dee8d81e90d37610042aa5edbb
Sébastien <sebtiz13@gmail.com>
2021-07-31 01:51:35 +0200

    feat(backend): implement paginated request

--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,19 @@
 import express from 'express'
 import dotenv from 'dotenv-flow'
-// import axios from './utils/axios.js'
-import fs from 'fs'
-import path from 'path'
+import axios from './utils/axios.js'
 
 dotenv.config()
 const app = express()
 const port = 3000
 
 app.get('/characters', async (req, res) => {
-  const exampleFile = path.join(process.cwd(), 'example.json')
-  const data = JSON.parse(fs.readFileSync(exampleFile, 'utf8'))
-  // const { data } = await axios.get('/characters')
+  const { page } = req.query
+
+  const { data } = await axios.get('/characters', {
+    params: {
+      offset: Number(page) * 20
+    }
+  })
   return res.header('Access-Control-Allow-Origin', '*')
     .json(data)
 })
