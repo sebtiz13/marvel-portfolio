6e2b88ce0518c04e4ef170ee373a30aa0237deff
Sébastien <sebtiz13@gmail.com>
2021-07-30 17:02:45 +0200

    feat(frontend): presentation for characters info

--- frontend/src/components/Character/CharacterInfo.vue
+++ frontend/src/components/Character/CharacterInfo.vue
@@ -1,15 +1,59 @@
 <template>
   <div class="characterInfo">
-    {{ description }}
-    {{ comics }}
-    {{ series }}
-    {{ stories }}
+    <ul class="characterInfo-chapter">
+      <li
+        :class="{ active: section === 'description' }"
+        @click.stop="changeSection('description')"
+      >
+        Description
+      </li>
+      <li
+        :class="{ active: section === 'comics' }"
+        @click.stop="changeSection('comics')"
+      >
+        Comics
+      </li>
+      <li
+        :class="{ active: section === 'series' }"
+        @click.stop="changeSection('series')"
+      >
+        Series
+      </li>
+      <li
+        :class="{ active: section === 'stories' }"
+        @click.stop="changeSection('stories')"
+      >
+        Stories
+      </li>
+    </ul>
+    <div class="characterInfo-section">
+      <span v-if="section === 'description'">
+        {{ description }}
+      </span>
+      <CharacterItems
+        v-if="section === 'comics'"
+        :items="comics.items"
+      />
+      <CharacterItems
+        v-if="section === 'series'"
+        :items="series.items"
+      />
+      <CharacterItems
+        v-if="section === 'stories'"
+        :items="stories.items"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import CharacterItems from '@/components/Character/CharacterItems.vue'
+
 export default {
   name: 'CharacterInfo',
+  components: {
+    CharacterItems,
+  },
   props: {
     description: {
       type: String,
@@ -28,6 +72,17 @@ export default {
       default: () => ({}),
     },
   },
+  data: () => ({
+    section: 'comics',
+  }),
+  methods: {
+    /**
+     * @param {string} section
+     */
+    changeSection (section) {
+      this.section = section
+    },
+  },
 }
 </script>
 
@@ -35,11 +90,38 @@ export default {
 .characterInfo {
   backface-visibility: hidden;
   transform: rotateY(180deg);
-  background-color: #151515;
-  color: rgb(229, 229, 229)
+  background-color: rgb(241, 241, 241);
+  color: #151515;
 }
 
 .characterInfo img {
   max-width: 100%;
+}
+
+.characterInfo-chapter {
+  display: flex;
+  padding: 0;
+  margin: 0;
+}
+
+.characterInfo-chapter li {
+  display: block;
+  height: 15px;
+  border-top: 5px solid transparent;
+  background-color: #151515;
+  color: #ffff;
+  padding: 10px 15px;
+  flex-grow: 1;
+  text-align: center;
+}
+
+.characterInfo-chapter li.active {
+  border-color: #e62429;
+}
+
+.characterInfo-section {
+  padding: 15px;
+  overflow-x: auto;
+  height: calc(100% - 70px);
 }
 </style>
---
+++ frontend/src/components/Character/CharacterItems.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <ul>
+      <li
+        v-for="item in items"
+        :key="item.name"
+      >
+        {{ item.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CharacterInfo',
+  props: {
+    items: {
+      type: Array,
+      default: () => ([]),
+    },
+  },
+}
+</script>
